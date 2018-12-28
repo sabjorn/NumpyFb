882915de543eaf2e442339459f4aadb9e391a2d1
sabjorn <info@sabjorn.net>
2018-12-28 11:35:24 -0800

    -

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
 from distutils.core import setup
 
 setup(
     name='NumpyFb',
     version=open('VERSION').read(),
     packages=['numpyfb',],
+    author='Steven A. Bjornson',
+    author_email='info@sabjorn.net',
+    url='https://github.com/sabjorn/NumpyFb',
     license='MIT License',
     long_description=open('README.md').read(),
 )
